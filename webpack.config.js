ff9c86b8f4cf28b9185c18635f88c46722dfc34a
arbielhyn <150289854+arbielhyn@users.noreply.github.com>
2024-07-07 14:03:49 -0500

    Trying to Add a Navbar

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const webpack = require("webpack");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+
+module.exports = {
+  mode: "production", // Set mode to 'production' for optimized builds
+  entry: "./src/index.js", // Entry point of your application
+  output: {
+    filename: "bundle.js", // Output bundle file name
+    path: path.resolve(__dirname, "dist"), // Output directory
+    publicPath: "/dist/", // Public path (used by webpack-dev-server)
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [require("tailwindcss"), require("autoprefixer")],
+              },
+            },
+          },
+        ],
+      },
+      // Add more rules for other file types (e.g., images)
+    ],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(), // Show progress during compilation
+    new WebpackManifestPlugin(), // Generate manifest file for assets
+  ],
+};
