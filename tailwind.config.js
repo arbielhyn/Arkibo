bbe6eda410b9886053c6f17bab9c1d893b2497cc
arbielhyn <150289854+arbielhyn@users.noreply.github.com>
2024-07-07 14:44:09 -0500

    having trouble with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    "./app/views/categories/index.html.erb",
+    "./app/views/products/index.html.erb",
+    "./app/views/products/show.html.erb",
+    "./app/views/layouts/application.html.erb",
+    "./app/views/shared/_header.html.erb",
+    // Add any other paths or files where Tailwind CSS classes are used
+  ],
   content: [],
   theme: {
     extend: {},
