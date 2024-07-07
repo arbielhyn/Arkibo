2af516f7b09b2f0ba94be6724a86ab5e18612ea9
arbielhyn <150289854+arbielhyn@users.noreply.github.com>
2024-07-07 13:52:13 -0500

    Added more products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,12 @@
 class ProductsController < ApplicationController
   def index
     @categories = Category.all
-    @products = Product.page(params[:page]).order(created_at: :desc)
+    if params[:category_id]
+      @category = Category.find(params[:category_id])
+      @products = @category.products.page(params[:page]).order(created_at: :desc)
+    else
+      @products = Product.page(params[:page]).order(created_at: :desc)
+    end
   end
 
   def show
